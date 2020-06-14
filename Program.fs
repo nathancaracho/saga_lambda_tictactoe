dce664c241e5c28e60e3af0dff8a10c7db9d095b
Nathan branco caracho <nathan.caracho@hotmail.com>
2020-06-14 20:56:26 -0300

    first commit

---
+++ Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open TicTacToe
+open TicTacToe.Turn
+
+[<EntryPoint>]
+let main argv =
+     let players = [|runTurn "X"; runTurn "O"|]
+
+     let failureRun run turn turnIndex failure =
+          printf "%s\n" failure
+          run turn turnIndex
+          
+
+     let rec run turn turnIndex = 
+         printTable turn.Table
+         match players.[turnIndex % 2] turn with
+         | Success result -> run result (turnIndex+1)
+         | Failure result -> failureRun run turn turnIndex result
+         | Won -> printf "The player win"
+         |>ignore
+
+     
+
+     run {Table = createTable; Position = 0} 0
+     0 // return an integer exit code
